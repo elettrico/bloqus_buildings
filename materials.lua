6d7302a1cc029200ad77dc3598a157477fe9439f
fmoretti <fmoretti@tecytal.com>
2017-11-01 00:49:33 -0300

    added new blocks, resolved some issues

--- materials.lua
+++ materials.lua
@@ -11,18 +11,33 @@ simple_nodes = {
     {"chipped bricks"   , "chipped_bricks", "bricks_chipped", ""},
     {"dark green bricks", "dark_green_bricks", "bricks_dark_green", ""},
     {"oblique bricks"   , "oblique_bricks", "bricks_oblique", ""},
-    {"square bricks"    , "square_bricks", "bricks_square", ""},
+    {"square bricks"    , "square_bricks" , "bricks_square", ""},
     {"vertical stone bricks" , "vertical_stone_bricks", "bricks_vertical_stone", ""},
+    {"natural bricks"   , "natural_bricks", "bricks_natural", ""},
+    {"old bricks"       , "old_bricks"    , "bricks_old", ""},
+    {"blue square bricks","blue_square_bricks", "bricks_square_blue", ""},
 
     {"tiled blocks"  , "tiled_blocks"  , "tiled_blocks"  , ""},
     {"rusty concrete", "rusty_concrete", "rusty_concrete_wall"  , "concrete"},
+    {"klinker"       , "klinker"       , "klinker", ""},
+    {"path"          , "path"          , "path", ""},
+    {"little columns", "little_columns", "little_columns", ""}, -- need a top texture
 
     {"blue cobble" , "blue_cobble", "cobble_blue", ""},
     {"color cobble", "color_cobble", "cobble_color", ""},
+    {"stones cobble","stones_cobble","cobble_stones", ""},
 
     {"copper plates" , "copper_plates", "plates_copper", ""},
     {"hexagon plates", "hexagon_plates", "plates_hexagon", ""},
     {"romboid plates", "romboid_plates", "plates_romboids", ""},
+    {"circular plates", "circular_plates", "plates_circular", ""},
+    {"irregular plates", "irregular_plates", "plates_irregular", ""},
+    
+    {"rocks 1", "rocks_1", "rocks1", ""},
+    {"rocks 2", "rocks_2", "rocks2", ""},
+    {"rocks 3", "rocks_3", "rocks3", ""},
+    {"rocks 4", "rocks_4", "rocks4", ""},
+    {"rocks 5", "rocks_5", "rocks5", ""},
 
     {"decorative circles"   , "decorative_circles", "decorative_circles", ""},
     {"decorative rectangles", "decorative_rectangles", "decorative_rectangles", ""},
@@ -33,6 +48,8 @@ simple_nodes = {
     {"bolted metal"      , "bolted_metal", "metal_bolted", ""},
     {"metal rusted plate", "metal_rusted_plate", "metal_rusted_plate", ""},
     {"rusty metal"       , "rusty_metal", "metal_rusty", ""},
+    {"metal plates dark" , "metal_plates_dark", "metal_plates_dark", ""},
+    {"ventilation grid"  , "ventilation_grid", "ventilation_grid", ""},
 
     {"blue and white mosaic"   , "mosaic_blue_white", "mosaic_blue_white", ""},
     {"diamonds mosaic"         , "diamonds_mosaic", "mosaic_diamonds", ""},
